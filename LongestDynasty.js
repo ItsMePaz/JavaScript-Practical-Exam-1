8545e1fd9c966e073ced7890ed3a25f2e4f9af72
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-07-09 13:17:01 +0800

    Finished Exam

--- LongestDynasty.js
+++ LongestDynasty.js
@@ -9,7 +9,7 @@ let dynastyReign = [
   { "Bon Dynasty": "MCDXLV" } /* 1445 */,
   { "Maiko Dynasty": "MDCLXIV" } /*  1664*/,
   { "Paul Dynasty": "MCMXLIX" } /*1949  */,
-  /* { "Andre Dynasty": "MMMXICX" } /*3119*/
+  { "Andre Dynasty": "MMMXICX" } /*3119*/,
 ];
 
 let romanToInteger = [
@@ -22,79 +22,113 @@ let romanToInteger = [
   (M = 1000),
 ];
 
-let dynastyValues = dynastyReign.map(Object.values);
+if (!(dynastyReign.length > 0)) {
+  dynastyResult.innerText = `No Data`;
 
-let mapThough = Array.prototype.concat.apply([], dynastyValues);
+  console.log("No Data");
+} else {
+  let dynastyValues = dynastyReign.map(Object.values);
 
-function convertYear(evaledSingleReign) {
-  let dynastyReignLength = [];
-  let year = evaledSingleReign[0];
-  while (evaledSingleReign.length > 0) {
-    if (evaledSingleReign[0] > evaledSingleReign[1]) {
-      year -= evaledSingleReign[1];
-      evaledSingleReign.shift();
-      dynastyReignLength.push(year);
-    } else if (evaledSingleReign[0] < evaledSingleReign[1]) {
-      year += evaledSingleReign[1];
-      evaledSingleReign.shift();
-      dynastyReignLength.push(year);
-    } else if ((evaledSingleReign[0] = evaledSingleReign[1])) {
-      year += evaledSingleReign[0];
-      evaledSingleReign.shift();
-      dynastyReignLength.push(year);
-    } else {
-      evaledSingleReign.shift();
+  let mapThough = Array.prototype.concat.apply([], dynastyValues);
+
+  function convertYear(evaledSingleReign) {
+    let dynastyReignLength = [];
+    let year = evaledSingleReign[0];
+    while (evaledSingleReign.length > 0) {
+      if (evaledSingleReign[0] > evaledSingleReign[1]) {
+        year -= evaledSingleReign[1];
+        evaledSingleReign.shift();
+        dynastyReignLength.push(year);
+        if (evaledSingleReign[0] / 10 > evaledSingleReign[1]) {
+          year = 99999;
+          evaledSingleReign = 0;
+        }
+      } else if (evaledSingleReign[0] < evaledSingleReign[1]) {
+        year += evaledSingleReign[1];
+        evaledSingleReign.shift();
+        dynastyReignLength.push(year);
+        if (evaledSingleReign[0] / 10 > evaledSingleReign[1]) {
+          year = 99999;
+          evaledSingleReign = 0;
+        }
+      } else if ((evaledSingleReign[0] = evaledSingleReign[1])) {
+        year += evaledSingleReign[0];
+        evaledSingleReign.shift();
+        dynastyReignLength.push(year);
+        if (evaledSingleReign[0] / 10 > evaledSingleReign[1]) {
+          year = 99999;
+          evaledSingleReign = 0;
+        }
+      } else {
+        evaledSingleReign.shift();
+      }
     }
+
+    return year;
   }
 
-  return year;
-}
-
-let arraySplitReverse = mapThough.map((single) => {
-  return single.split("").reverse();
-});
-
-let arrayValues = arraySplitReverse.map((each) => {
-  return each.map(eval);
-});
-
-function longestdynasty() {
-  let listOfYears = []; //an array of year decimals
-  let arrayOfYears = arrayValues.map((singleYear) => {
-    return convertYear(singleYear);
+  let arraySplitReverse = mapThough.map((single) => {
+    return single.split("").reverse();
   });
 
-  listOfYears.push(arrayOfYears);
+  let arrayValues = arraySplitReverse.map((each) => {
+    return each.map(eval);
+  });
 
-  let yearsOrder = listOfYears[0];
-  yearsOrder.unshift(1000);
-  let dummy = [];
+  function longestdynasty() {
+    let listOfYears = []; //an array of year decimals
+    let arrayOfYears = arrayValues.map((singleYear) => {
+      return convertYear(singleYear);
+    });
 
-  let difference = "";
-  while (yearsOrder.length > 1) {
-    difference = Math.abs(yearsOrder[0] - yearsOrder[1]);
+    listOfYears.push(arrayOfYears);
 
-    dummy.push(difference);
+    let yearsOrder = listOfYears[0];
+    yearsOrder.unshift(1000);
+    let dummy = [];
 
-    yearsOrder.shift();
+    let difference = "";
+    while (yearsOrder.length > 1) {
+      difference = Math.abs(yearsOrder[0] - yearsOrder[1]);
+
+      dummy.push(difference);
+
+      yearsOrder.shift();
+    }
+
+    let dynastyKeys = dynastyReign.map(Object.keys);
+    let dynastyKeysArray = Array.prototype.concat.apply([], dynastyKeys);
+
+    let dynastyDecimalValues = dummy;
+
+    let merged = dynastyKeysArray.reduce(
+      (obj, key, index) => ({ ...obj, [key]: dynastyDecimalValues[index] }),
+      {}
+    );
+
+    let invalids = Object.keys(merged).find((key) => merged[key] > 9999);
+
+    let invalidYear = merged[invalids];
+
+    dynastyDecimalValues = dynastyDecimalValues.filter(function (item) {
+      return item !== invalidYear;
+    });
+
+    let longestReignYear = Math.max(...dynastyDecimalValues);
+
+    let longestDynasty = Object.keys(merged).find(
+      (key) => merged[key] === longestReignYear
+    );
+
+    console.log(`${invalids} is Invalid`);
+    console.log(
+      `${longestDynasty} is the longest dynasty among the list with ${merged[longestDynasty]} years`
+    );
+    dynastyResult.innerText = `${longestDynasty} is the longest dynasty among the list with ${merged[longestDynasty]} years`;
+
+    delete merged[invalids];
+    console.log(merged);
   }
 
-  let dynastyKeys = dynastyReign.map(Object.keys);
-  let dynastyKeysArray = Array.prototype.concat.apply([], dynastyKeys);
-
-  let dynastyDecimalValues = dummy;
-
-  const merged = dynastyKeysArray.reduce(
-    (obj, key, index) => ({ ...obj, [key]: dynastyDecimalValues[index] }),
-    {}
-  );
-  console.log(merged);
-  let longestReignYear = Math.max(...dynastyDecimalValues);
-  let longestDynasty = Object.keys(merged).find(
-    (key) => merged[key] === longestReignYear
-  );
-  console.log(longestDynasty);
-  dynastyResult.innerText = longestDynasty;
+  longestdynasty();
 }
-
-longestdynasty();
